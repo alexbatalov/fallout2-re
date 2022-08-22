082a3970362cd2061400a9bac19fda0bc00eb241
Alexander Batalov <alex.batalov@gmail.com>
2022-08-22 15:20:43 +0300

    Decompile _deleteChar

--- src/widget.c
+++ src/widget.c
@@ -1,7 +1,17 @@
 #include "widget.h"
 
+#include <string.h>
+
 // 0x66E6A0
 int _updateRegions[32];
+
+// 0x4B45A0
+void _deleteChar(char* string, int pos, int length)
+{
+    if (length > pos) {
+        memcpy(string + pos, string + pos + 1, length - pos);
+    }
+}
 
 // 0x4B5A64
 void _showRegion(int a1)
--- src/widget.h
+++ src/widget.h
@@ -3,6 +3,7 @@
 
 extern int _updateRegions[32];
 
+void _deleteChar(char* string, int pos, int length);
 void _showRegion(int a1);
 int _update_widgets();
 void sub_4B5998(int win);
