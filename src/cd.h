119a023efaea3d714959bb390029ef6c021af429
Alexander Batalov <alex.batalov@gmail.com>
2022-09-02 12:31:23 +0300

    Decompile cd functions

---
+++ src/cd.h
@@ -0,0 +1,10 @@
+#ifndef CD_H
+#define CD_H
+
+int sub_420B10(const char* a1);
+int sub_420B28(const char* a1, const char* a2);
+int sub_420B8C(const char* a1);
+int sub_420BDC(const char* a1, int a2);
+int sub_420C18(const char* a1, const char* a2, int a3);
+
+#endif /* CD_H */
