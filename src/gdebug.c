27e7165449d942dc5f17c4b1d4c413d88bb995ab
Alexander Batalov <alex.batalov@gmail.com>
2022-09-02 13:41:31 +0300

    Decompile fatal_error

---
+++ src/gdebug.c
@@ -0,0 +1,26 @@
+#include "gdebug.h"
+
+#include <stdlib.h>
+
+#include "debug.h"
+#include "window_manager.h"
+
+// 0x444C90
+void fatal_error(const char* format, const char* message, const char* file, int line)
+{
+    char stringBuffer[260];
+
+    debugPrint("\n");
+    debugPrint(format, message, file, line);
+
+    windowManagerExit();
+
+    printf("\n\n\n\n\n   ");
+    printf(format, message, file, line);
+    printf("\n\n\n\n\n");
+
+    sprintf(stringBuffer, format, message, file, line);
+    showMesageBox(stringBuffer);
+
+    exit(1);
+}
