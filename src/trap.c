d24d72baeca655d275bdba27bacd4f09a7e73b88
Alexander Batalov <alex.batalov@gmail.com>
2022-09-27 12:19:02 +0300

    Decompile trap_report

--- src/trap.c
+++ src/trap.c
@@ -5,6 +5,13 @@
 #include "debug.h"
 #include "window_manager.h"
 
+typedef struct TrapEntry {
+    int address;
+    int name;
+    int field_8;
+    int field_C;
+} TrapEntry;
+
 typedef struct DuplicateEntry {
     int address;
     int field_4;
@@ -16,6 +23,11 @@ typedef struct DuplicateEntry {
 static void duplicate_report(int trap, int offset, const char* file, int line);
 static void heap_report(int trap, int address, const char* file, int line);
 
+// NOTE: Probably |trap_list_data|.
+//
+// 0x51DC44
+static TrapEntry* off_51DC44 = NULL;
+
 // NOTE: Probably |trap_list_duplicate|.
 //
 // 0x66BE48
@@ -26,6 +38,23 @@ static DuplicateEntry stru_66BE48[512];
 // 0x4B4190
 void _trap_init()
 {
+}
+
+// 0x4B43A4
+static void trap_report(int trap, int address, const char* file, int line)
+{
+    TrapEntry* entry;
+
+    entry = &(off_51DC44[trap]);
+    debugPrint("TRAPPED A STOMP ERROR:\n");
+    debugPrint("Stomp caught by check on line %d", line);
+    debugPrint(" of module %s.\n", file);
+    debugPrint("Data stomped was in trap %s", entry->name);
+    debugPrint(" at address %p.\n", entry->address);
+    debugPrint("See comment in trap.c for suggestions on better");
+    debugPrint(" isolating the stomp bug.\n");
+    showMesageBox("STOMPED!");
+    exit(1);
 }
 
 // 0x4B4430
